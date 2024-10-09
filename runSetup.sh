f0866474b3f26c45d951a5ad345b8a5c724bacc0
EmanuelsGiT <emanuelmonteiro2002@gmail.com>
2024-10-09 15:02:39 +0100

    script atualizado

--- runSetup.sh
+++ runSetup.sh
@@ -1,16 +1,26 @@
 echo "Setting up dependencies..."
 
+echo "#### Installing Base-devel"
+sudo pacman -S base-devel --noconfirm
+
+echo "#### Installing yay"
+sudo pacman -S yay --noconfirm
+
 echo "#### Installing Git"
-sudo apt install git
+sudo pacman -S git --noconfirm
 
 echo "#### Installing CMake"
-sudo apt install cmake
+sudo pacman -S cmake --noconfirm
 
 echo "#### Installing lm-sensors"
-sudo apt install lm-sensors
+sudo pacman -S lm_sensors --noconfirm
 
 echo "### Installing Sloc Cloc and Code (scc)"
-sudo snap install scc
+git clone https://github.com/boyter/scc.git
+cd scc
+go build
+sudo cp scc /usr/local/bin/
+cd ..
 
 echo "#### Installing Powercap"
 git clone https://github.com/powercap/powercap.git
@@ -19,13 +29,13 @@ mkdir _build
 cd _build
 cmake ..
 make
-make install
+sudo make install
 
 cd ..
 cd ..
 
 echo "#### Installing package config"
-sudo apt install pkg-config
+sudo pacman -S pkgconf --noconfirm
 
 echo "#### Installing Raplcap"
 git clone https://github.com/powercap/raplcap.git
@@ -34,7 +44,39 @@ mkdir _build
 cd _build
 cmake ..
 make
-make install
+sudo make install
 
 cd ..
 cd ..
+
+echo "#### Installing Python"
+sudo pacman -S python --noconfirm
+
+echo "#### Installing Python Benchmark"
+yay -S python-pyperformance --noconfirm
+
+echo "#### Installing Haskell"
+sudo pacman -S ghc --noconfirm
+
+echo "#### Installing Haskell Benchmark"
+git clone https://github.com/ghc/nofib.git
+
+echo "#### Installing Java"
+sudo pacman -S jdk-openjdk --noconfirm
+
+echo "#### Installing Java Benchmark"
+git clone https://github.com/dacapobench/dacapobench.git
+sudo pacman -S apache-ant
+cd dacapobench
+ant
+sudo cp ./jar/dacapo-9.12-MR1.jar /usr/local/bin/dacapo.jar
+echo 'alias dacapo="java -jar /usr/local/bin/dacapo.jar"' >> ~/.bashrc
+source ~/.bashrc
+
+echo "#### Installing Go"
+sudo pacman -S go --noconfirm
+
+echo "#### Installing Ruby"
+sudo pacman -S ruby --noconfirm
+
+
