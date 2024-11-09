9282bdd9d30f9a6645fe1e2de9868729f6255009
EmanuelsGiT <emanuelmonteiro2002@gmail.com>
2024-11-09 14:52:39 +0000

    C corrigido e GO?

--- RAPL/main.c
+++ RAPL/main.c
@@ -15,7 +15,7 @@
 #include <sys/time.h>
 
 
-#define TEMPERATURETHRESHOLD 34.0
+#define TEMPERATURETHRESHOLD 44.66666793823242
 #define VARIANCE 8 // 5
 #define WHATTSCAP -1
 #define MAX_STRING_LENGTH 500
